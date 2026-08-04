63f7f36932c1340cb76e9e9bd7629c7dc7e6740b
Civi <19486398+angribot@users.noreply.github.com>
2026-08-04 16:10:05 +0800

    fix: fix type error

--- linux/fish/config.fish
+++ linux/fish/config.fish
@@ -4,5 +4,5 @@ if status is-interactive
     fish_config theme choose everforest
     fish_vi_key_bindings
     starship init fish | source
-    zoxide init --cmd cd fish | source
+    zoxide init fish --cmd cd| source
 end
