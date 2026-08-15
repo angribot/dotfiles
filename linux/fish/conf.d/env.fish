70326438f5e4d5c6940e075484be8770efb4a065
Civi <19486398+angribot@users.noreply.github.com>
2026-08-15 17:05:36 +0800

    fix: set LANG to C.UTF-8

--- linux/fish/conf.d/env.fish
+++ linux/fish/conf.d/env.fish
@@ -1,6 +1,6 @@
 set -x COLORTERM truecolor
 set -x GPG_TTY (tty)
-# set -x LANG "en_US.UTF-8"
+set -x LANG "C.UTF-8"
 set -x LESS "-R --use-color --ignore-case --jump-target=4 --LONG-PROMPT --no-init --quit-if-one-screen"
 set -x PAGER bat
 # set -x XDG_CONFIG_HOME "$HOME/.config"
