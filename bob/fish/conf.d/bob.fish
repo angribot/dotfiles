8a38543493c7cc73c44d386212ddf7cee4f55f2c
Civi <19486398+angribot@users.noreply.github.com>
2026-08-23 14:50:08 +0800

    chore: remove bob and rustup

--- bob/fish/conf.d/bob.fish
+++
@@ -1,4 +0,0 @@
-# Credit: https://github.com/rust-lang/rustup/blob/master/src/cli/self_update/env.fish
-if not contains "$HOME/.local/share/bob/nvim-bin" $PATH
-    set -x PATH "$HOME/.local/share/bob/nvim-bin" $PATH
-end
--- rustup/fish/conf.d/rustup.fish
+++
@@ -1,7 +0,0 @@
-# rustup shell setup
-set -x RUSTUP_HOME "$HOME/.local/share/rustup"
-set -x CARGO_HOME "$HOME/.local/share/cargo"
-if not contains "$CARGO_HOME/bin" $PATH
-    # Prepending path in case a system-installed rustc needs to be overridden
-    set -x PATH "$CARGO_HOME/bin" $PATH
-end
